edefc63f43a0478fbcb21080116787d553c0ce6d
Yusima523 <matthewralphlee@gmail.com>
2021-12-31 18:50:42 +0900

    ドーナツグラフを作成

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Header } from "./Views/Components/Header/Header";
 import { Donut } from "./Views/Components/Donut/Donut";
+import { Project } from "./Views/Components/Project/Project";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Donut calorie={90} />
+      <Project
+        taskName={"Tascook"}
+        completedTask={8}
+        taskSum={12}
+      />
     </div>
   );
 }
