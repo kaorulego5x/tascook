fb3c31dac29e85890c62e8ba59e28b1364a8d71a
kaorulego5x <kaorulego5x@gmail.com>
2021-12-31 12:48:18 +0900

    ファイル構造を修正

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Header } from "./Components/Header";
+import { Header } from "./Views/Components/Header/Header";
 
 function App() {
   return (
