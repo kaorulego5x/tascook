75f858723d58aaeb0d9d4f7d568e47106ac628c0
Yusima523 <matthewralphlee@gmail.com>
2021-12-31 17:59:59 +0900

    ドーナツグラフのコンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Header } from "./Views/Components/Header/Header";
+import { Donut } from "./Views/Components/Donut/Donut";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Donut calorie={90} />
     </div>
   );
 }
